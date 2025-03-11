190986275195b848709b53986b13ba7f6dc1416c
Adekola Toniloba <atoniloba@gmail.com>
2025-03-11 22:40:49 +0100

    added test db to test database connectivity

---
+++ app/api/test-db/route.ts
@@ -0,0 +1,22 @@
+// app/api/test-db/route.ts
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    await prisma.$connect();
+    return NextResponse.json({ message: "Database connected" });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error occurred";
+
+    return NextResponse.json(
+      { error: `Database connection failed: ${errorMessage}` },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
